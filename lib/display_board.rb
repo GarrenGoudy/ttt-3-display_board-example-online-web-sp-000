4a7a6f72fdb5a0fdf920085eb9bbad2a98b0835a
Garren Goudy <garren.goudy@yahoo.com>
2021-06-02 14:54:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  print row[1]  {"   |   |   "}
+  print row1  {"   |   |   "}
   print "------------"
   print {"   |   |   "}
   print "------------"
   print {"   |   |   "}
 end 
 
-def row[1] = ["   ", "   ", "   "]
+def row1 = ["   ", "   ", "   "]
